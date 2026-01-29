42eddb824f1848b171b00f023943a6e8a5f83993
kaviya <nkaviya.doodleblue@gmail.com>
2026-01-29 18:00:39 +0530

    Initial commit

--- index.js
+++ index.js
@@ -1,12 +1,18 @@
+  GNU nano 6.3                                                                      index.js                                                                      Modified
 /**
  * Copyright 2013-2022 the PM2 project authors. All rights reserved.
  * Use of this source code is governed by a license that
  * can be found in the LICENSE file.
  */
+const express = require('express');
+const app = express();
 
-process.env.PM2_PROGRAMMATIC = 'true';
+const PORT = 3000;
 
-var API = require('./lib/API.js');
+app.get('/', (req, res) => {
+  res.send('Hello! Node.js app is running with PM2 🚀');
+});
 
-module.exports = new API;
-module.exports.custom = API;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
